a8c514f9e4d2767ba84d09233deb773987ae7669
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-06-04 21:56:44 +0530

    starting with the script

---
+++ Projects/project-frontendjs/script.js
@@ -0,0 +1,16 @@
+const redBox = document.getElementsByClassName("red-box");
+const greenBox = document.getElementsByClassName("green-box");
+const blueBox = document.getElementsByClassName("blue-box");
+const yellowBox = document.getElementsByClassName("yellow-box");
+
+const inputBox = document.getElementsByClassName("input-area");
+
+const addUser = document.getElementById("add-user");
+const deleteUser = document.getElementById("delete-user");
+const moveToRed = document.getElementById("move-to-red");
+const moveToGreen = document.getElementById("move-to-green");
+const moveToBlue = document.getElementById("move-to-blue");
+const moveToYellow = document.getElementById("move-to-yellow");
+
+console.log(redBox);
+console.log(moveToBlue);
