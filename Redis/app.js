398841d50f12a1f167e12da4a7f578b6ea53995e
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-06-11 14:39:48 +0530

    redis added

---
+++ Redis/app.js
@@ -0,0 +1,15 @@
+const redis = require("redis");
+
+const redisURL = "redis://127.0.0.1:6379";
+
+const client = redis.createClient(redisURL);
+
+client.set("name", "redis");
+console.log("Data is set now");
+
+client.get("name", (err, value) => {
+  if (err) {
+    console.log("Error: " + err);
+  }
+  console.log(value);
+});
