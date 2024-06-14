79692a9a30aea6b07d9e38b8c8552d3ffd353cae
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-06-14 13:02:54 +0530

    nodejs1 file added

--- NodeJS/workshop1/app.js
+++ NodeJS/workshop1/app.js
@@ -2,4 +2,31 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  //  console.log(req);
+  res.send("I am response");
+});
+
+app.get("/home", (req, res) => {
+  res.send("I am home");
+});
+
+app.get("/add", (req, res) => {
+  let n1 = 2;
+  let n2 = 1;
+  let sum = n1 + n2;
+
+  res.send(`Sum is ${sum}`);
+});
+
+app.post("/add", (req, res) => {
+  let n1 = req.body.num1;
+  let n2 = req.body.num2;
+  let sum = n1 + n2;
+
+  res.send(`Sum is ${sum}`);
+});
+
 app.listen(3000);
