8a7b4cee6a235333f32d59b8498d7ed658cd1938
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-06-15 16:03:22 +0530

    sequelize initiated and created its model

---
+++ NodeJS/workshop3/models/user.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncreament: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  remarks: STRING,
+});
+
+module.exports = User;
