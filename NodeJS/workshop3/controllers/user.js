0062ba519f2e25a5c00655b9f35f8b03c6c87147
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-06-16 12:18:48 +0530

    completed CRUD operation using sequelize

--- NodeJS/workshop3/controllers/user.js
+++ NodeJS/workshop3/controllers/user.js
@@ -2,44 +2,47 @@ const userModel = require("../models/user");
 
 // register
 module.exports.register = async (req, res) => {
-  let result = await userModel.create(req.body);
-
-  if (result.dataValues.id > 0) {
+  try {
+    let result = await userModel.create(req.body);
     res.send({ status: "success", data: { id: result.dataValues.id } });
-  } else {
+  } catch (err) {
     res.send({ status: "error", message: "User registration failed" });
   }
 };
 
 // get
 module.exports.get = async (req, res) => {
-  let data = await userModel.get(req.body);
-
-  if (data) {
+  try {
+    let data = await userModel.findByPk(req.body.id);
     res.send({ status: "success", data: data });
-  } else {
+  } catch (err) {
     res.send({ status: "error", message: "User not found" });
   }
 };
 
 // update
 module.exports.update = async (req, res) => {
-  let status = await userModel.update(req.body);
+  try {
+    let user = await userModel.findByPk(req.body.id);
+    user.password = req.body.password;
+    await user.save();
 
-  if (status) {
     res.send({ status: "success", message: "user data updated" });
-  } else {
+  } catch (err) {
     res.send({ status: "error", message: "User not updated" });
   }
 };
 
 // delete
 module.exports.delete = async (req, res) => {
-  let status = await userModel.delete(req.body);
+  try {
+    let user = await userModel.findByPk(req.body.id);
+    await user.destroy();
 
-  if (status) {
     res.send({ status: "success", message: "user deleted" });
-  } else {
+  } catch (err) {
     res.send({ status: "error", message: "User not deleted" });
   }
+
+  let status = await userModel.delete(req.body);
 };
