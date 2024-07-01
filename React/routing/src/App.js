bd75498bfb277d8758ee54d8ce1cf8fea7a9203e
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-07-01 10:38:55 +0530

    router link code added

--- React/routing/src/App.js
+++ React/routing/src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import MainMenu from "./components/layouts/MainMenu";
 
 function App() {
   return (
     <BrowserRouter>
+      <MainMenu />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
