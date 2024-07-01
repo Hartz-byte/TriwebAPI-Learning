3e9a68966877737c47b56bb80f328012d6f83698
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-07-01 09:55:51 +0530

    react first project

---
+++ React/dummy/src/App.js
@@ -0,0 +1,11 @@
+import FirstComponent from "./components/FirstComponent";
+
+function App() {
+  return (
+    <div>
+      <FirstComponent name="Harsh" reason="demo" />
+    </div>
+  );
+}
+
+export default App;
