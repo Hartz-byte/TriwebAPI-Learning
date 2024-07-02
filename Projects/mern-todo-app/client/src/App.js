c7454c4b858927b8d4d317be34792dad51e80643
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-07-02 14:33:01 +0530

    mern-todo done

--- Projects/mern-todo-app/client/src/App.js
+++ Projects/mern-todo-app/client/src/App.js
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./components/Homepage";
+import HomePage from "./components/HomePage";
 import AddUser from "./components/AddUser";
 import EditUser from "./components/EditUser";
 import "./App.css";
@@ -7,8 +7,9 @@ import "./App.css";
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <HomePage /> },
-    { path: "/", element: <AddUser /> },
-    { path: "/", element: <EditUser /> },
+    { path: "/add-user", element: <AddUser /> },
+    { path: "/edit-user", element: <EditUser /> },
+    { path: "*", element: <div>PAGE NOT FOUND</div> },
   ]);
   return <RouterProvider router={router} />;
 }
