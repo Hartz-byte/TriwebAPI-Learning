c701850d43da9ae6a82639a74e62be6b9dd706b3
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-07-03 23:58:03 +0530

    connection error handling

--- Projects/QuizManager/backend/src/controllers/user.ts
+++ Projects/QuizManager/backend/src/controllers/user.ts
@@ -1,10 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 
-const registerUser = (req: Request, res: Response) => {
-  console.log("data: ", req.body);
+import User from "../models/user";
 
-  console.log("registeration done");
-  res.send("registeration done");
+interface ReturnResponse {
+  status: "success" | "error";
+  message: String;
+  data: {};
+}
+
+const registerUser = async (req: Request, res: Response) => {
+  let resp: ReturnResponse;
+
+  try {
+    const user = new User(req.body);
+    const result = await user.save();
+
+    if (!result) {
+      resp = { status: "error", message: "No result dound", data: {} };
+
+      res.send(resp);
+    } else {
+      resp = {
+        status: "success",
+        message: "Registration done",
+        data: { userId: result._id },
+      };
+
+      res.send(resp);
+    }
+  } catch (err) {
+    resp = { status: "error", message: "Something went wrong", data: {} };
+    res.status(500).send(resp);
+  }
 };
 
 export { registerUser };
