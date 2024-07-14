749732fbd8c3da0cda03b041d06264b00c0e7dc1
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-07-14 13:48:47 +0530

    CRUD functions done

--- Projects/QuizManager/backend/src/controllers/quiz.ts
+++ Projects/QuizManager/backend/src/controllers/quiz.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
+import ProjectError from "../helper/error";
 import Quiz from "../models/quiz";
 
 interface ReturnResponse {
@@ -30,20 +31,122 @@ const createQuiz = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-const getQuiz = (req: Request, res: Response) => {
-  res.send(req.params.quizId);
+const getQuiz = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const quizId = req.params.quizId;
+
+    const quiz = await Quiz.findById(quizId, {
+      name: 1,
+      questions_list: 1,
+      answers: 1,
+      created_by: 1,
+    });
+
+    if (!quiz) {
+      const err = new ProjectError("Quiz not found.");
+      err.statusCode = 404;
+      throw err;
+    }
+
+    console.log(quiz);
+
+    if (req.userId !== quiz.created_by) {
+      const err = new ProjectError("You are not authorized.");
+      err.statusCode = 403;
+      throw err;
+    }
+
+    const resp: ReturnResponse = {
+      status: "success",
+      message: "quiz created successfully",
+      data: quiz,
+    };
+
+    res.status(200).send(resp);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const updateQuiz = (req: Request, res: Response) => {
-  res.send(req.body);
+const updateQuiz = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const quizId = req.body._id;
+    const quiz = await Quiz.findById(quizId);
+
+    if (!quiz) {
+      const err = new ProjectError("Quiz not found.");
+      err.statusCode = 404;
+      throw err;
+    }
+
+    quiz.name = req.body.name;
+    quiz.questions_list = req.body.questions;
+    quiz.answers = req.body.answers;
+
+    await quiz.save();
+
+    const resp: ReturnResponse = {
+      status: "success",
+      message: "quiz updated successfully",
+      data: {},
+    };
+
+    res.status(200).send(resp);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteQuiz = (req: Request, res: Response) => {
-  res.send(req.params.quizId);
+const deleteQuiz = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const quizId = req.params.quizId;
+
+    const quiz = await Quiz.deleteOne({ _id: quizId });
+
+    if (!quiz) {
+      const err = new ProjectError("Quiz not found.");
+      err.statusCode = 404;
+      throw err;
+    }
+
+    const resp: ReturnResponse = {
+      status: "success",
+      message: "quiz deleted successfully",
+      data: {},
+    };
+
+    res.status(200).send(resp);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const publishQuiz = (req: Request, res: Response) => {
-  res.send(req.body);
+const publishQuiz = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const quizId = req.params.quizId;
+
+    const quiz = await Quiz.findById(quizId);
+
+    if (!quiz) {
+      const err = new ProjectError("Quiz not found.");
+      err.statusCode = 404;
+      throw err;
+    }
+
+    quiz.is_published = true;
+    await quiz.save();
+
+    const resp: ReturnResponse = {
+      status: "success",
+      message: "quiz published successfully",
+      data: {},
+    };
+
+    res.status(200).send(resp);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 export { createQuiz, getQuiz, updateQuiz, deleteQuiz, publishQuiz };
