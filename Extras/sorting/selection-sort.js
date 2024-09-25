1459c4d02d59f4004cac756986acb7f3d8db7f7b
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-09-25 13:04:45 +0530

    techniques of sorting

---
+++ Extras/sorting/selection-sort.js
@@ -0,0 +1,16 @@
+function selectionSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+    [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+  }
+  return arr;
+}
+
+console.log(selectionSort([64, 25, 12, 22, 11]));
